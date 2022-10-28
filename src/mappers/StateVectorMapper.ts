4db91f1541828a93af0095f27adba1154d825534
Raed667 <github@raed.email>
2022-10-28 17:20:43 +0200

    test: add jest and basic tests

--- src/mappers/StateVectorMapper.ts
+++ src/mappers/StateVectorMapper.ts
@@ -7,10 +7,9 @@ export const stateVectorMapper = (raw: any[]): StateVector => {
 
   return {
     icao24: raw[0],
-    callsign: raw?.[1].trim(),
+    callsign: raw[1]?.trim(),
     originCountry: raw[2],
     lastPositionUpdate: raw[3],
-
     lastContact: raw[4],
     longitude: raw[5],
     latitude: raw[6],
@@ -18,7 +17,6 @@ export const stateVectorMapper = (raw: any[]): StateVector => {
     onGround: raw[8],
     velocity: raw[9],
     heading: raw[10],
-
     verticalRate: raw[11],
     serials: raw[12],
     geoAltitude: raw[13],
